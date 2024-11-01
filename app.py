d79f1b90dcb8d47b48f3df05c984257ae7640fc1
hndrr <o_honda@go-spiral.ai>
2024-11-01 16:26:47 +0900

    Add .gitignore, requirements.txt, app.py, and config.py files

---
+++ app.py
@@ -0,0 +1,6 @@
+import config
+from langchain_openai import OpenAI
+
+model = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0)
+output = model.invoke("自己紹介をしてください")
+print(output)
---
+++ config.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+# Load environment variables
+load_dotenv()
+
+# Get environment variables
+LANGCHAIN_TRACING_V2 = os.getenv("LANGCHAIN_TRACING_V2")
+LANGCHAIN_ENDPOINT= os.getenv("LANGCHAIN_ENDPOINT")
+LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
+LANGCHAIN_PROJECT = os.getenv("LANGCHAIN_PROJECT")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
